2a8637835a29700f86196921a200e9a09c08a998
juliohdzrj <julio.hdzrj@gmail.com>
2025-03-18 09:44:20 -0600

    config env develop and test

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,12 @@
 const Sequelize = require('sequelize');
+const config = require('config');
 
-const sequelize = new Sequelize('hoaxify', 'my-db-user', 'db-p4ss', {
-  dialect: 'sqlite',
-  storage: './database.sqlite',
-  logging: false,
+const dbconfig = config.get('database');
+
+const sequelize = new Sequelize(dbconfig.database, dbconfig.username, dbconfig.password, {
+  dialect: dbconfig.dialect,
+  storage: dbconfig.storage,
+  logging: dbconfig.logging,
 });
 
 module.exports = sequelize;
